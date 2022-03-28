55f45cea467a179847b656dc8bb8ae7fd37f0907
Jmatop <91370388+Jmatop@users.noreply.github.com>
2022-03-28 18:39:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ sudo apt update
 #Instalamos Suricata
 sudo add-apt-repository ppa:oisf/suricata-stable && sudo apt install --yes suricata
 
-#sudo apt install --yes software-properties-common
-#sudo apt install --yes suricata
+sudo apt install --yes software-properties-common
+sudo apt install --yes suricata
 
 #Habilitamos Suricata
 sudo systemctl enable suricata.service
