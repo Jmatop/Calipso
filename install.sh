240b8884b7b106f74108e870847a678c092569a0
Jmatop <91370388+Jmatop@users.noreply.github.com>
2022-03-29 17:41:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-red='\033[0;31m'
-green='\033[0;32m'
-yellow='\033[0;33m'
-cyan='\033[0;36m'
-clear='\033[0m'
 
-echo -e "{$cyan}"
 #Actualizamos librerias
 sudo apt update
-echo -e "{$clear}"
 
-echo -e "{$red}"
+
+
 #Instalamos Suricata
 sudo add-apt-repository ppa:oisf/suricata-stable 
 sudo apt install --yes software-properties-common
@@ -20,9 +14,7 @@ sudo apt install --yes suricata
 #Habilitamos Suricata
 #sudo systemctl enable suricata.service
 #sudo suricata-update
-echo -e "{$clear}"
 
-echo -e "{$yellow}"
 #Instalamos servidor Apache
 sudo apt install --yes apache2
 
@@ -35,9 +27,7 @@ sudo apt install -y yara
 git clone https://github.com/Yara-Rules/rules.git
 #Para ejecutar las reglas Yara el comando que se usa es:
 #yara -f /home/rules/index.yar /home/calipso/
-echo -e "{$clear}"
 
-echo -e "{$green}"
 #Instalamos el IP Tracer
 git clone https://github.com/rajkumardusad/IP-Tracer.git
 cd IP-Tracer
@@ -45,5 +35,4 @@ chmod +x install
 ./install
 #Ejemplo comandos IP Tracer:
 #trace -t dirección ip
-echo -e "{$clear}"
 
