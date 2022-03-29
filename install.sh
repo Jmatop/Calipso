eb42fdd23f1c07a764a059d40805f0e0ff5f3e5c
Jmatop <91370388+Jmatop@users.noreply.github.com>
2022-03-29 17:14:02 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@
 sudo apt update
 
 #Instalamos Suricata
-sudo add-apt-repository ppa:oisf/suricata-stable && sudo apt install --yes suricata
+sudo add-apt-repository ppa:oisf/suricata-stable 
 sudo apt install --yes software-properties-common
 sudo apt install --yes suricata
 
 #Habilitamos Suricata
-sudo systemctl enable suricata.service
-sudo suricata-update
+#sudo systemctl enable suricata.service
+#sudo suricata-update
 
 #Instalamos servidor Apache
 sudo apt install --yes apache2
