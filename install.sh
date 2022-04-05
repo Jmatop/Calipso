e8bbfe8964da02b9f13b2213527d8d2134eb4638
Jmatop <91370388+Jmatop@users.noreply.github.com>
2022-04-05 19:57:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -46,6 +46,7 @@ sudo apt-get -qq --yes install podman
 
 #Configuración OpenSearch
 podman pod create -n miOS -p 9200:9200 -p 9600:9600 -p 5601:5601
+sleep 30
 podman container create --pod=miOS -e "discovery.type=single-node" opensearchproject/opensearch:latest
 podman container create --pod=miOS -e "opensearch.username=admin" -e "opensearch.password=admin" -e "opensearch.ssl.verificationMode=none" opensearchproject/opensearch-dashboards:latest
 podman pod start miOS
