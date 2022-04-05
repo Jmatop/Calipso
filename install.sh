b4c7264ada37c73a649850d3b336b43de349e53e
Jmatop <91370388+Jmatop@users.noreply.github.com>
2022-04-05 17:49:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,6 +35,15 @@ chmod +x install
 ./install
 #Ejemplo comandos IP Tracer:
 #trace -t dirección ip
+
+#Instalación OpenSearch
+source /etc/os-release
+echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key | sudo apt-key add -
+apt-get update
+sudo apt install -y podman
+
+
 echo "Ready."
 sleep 5 
 clear
