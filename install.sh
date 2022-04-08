62fa2b55839bbab8ffb21b7df3bcbd97f3c45735
Jmatop <91370388+Jmatop@users.noreply.github.com>
2022-04-08 16:01:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,35 +2,36 @@
 
 
 echo "$(tput setaf 6)Actualizamos librerias "
-tput reset
 sleep 3
+tput reset
 sudo apt update
 
 
 
 echo "$(tput setaf 4)Instalamos Suricata"
-tput reset
 sleep 3
+tput reset
 sudo add-apt-repository ppa:oisf/suricata-stable 
 sudo apt install --yes software-properties-common
 sudo apt install --yes suricata
 
 
 echo "$(tput setaf 5)Habilitamos Suricata"
+sleep 3
 tput reset
 sudo systemctl enable suricata.service
 sudo suricata-update
 
 
 echo "$(tput setaf 6)Instalamos servidor Apache"
-tput reset
 sleep 3
+tput reset
 sudo apt install --yes apache2
 
 
 echo "$(tput setaf 2)Instalamos las dependencias para Yara"
-tput reset
 sleep 3
+tput reset
 sudo apt-get install -y automake libtool make gcc flex bison libssl-dev libjansson-dev libmagic-dev
 sudo apt-get install -y checkinstall
 sudo checkinstall -y --deldoc=yes
@@ -42,8 +43,8 @@ git clone https://github.com/Yara-Rules/rules.git
 
 
 echo "$(tput setaf 3)Instalamos el IP Tracer"
-tput reset
 sleep 3
+tput reset
 git clone https://github.com/rajkumardusad/IP-Tracer.git
 cd IP-Tracer
 chmod +x install
@@ -53,8 +54,8 @@ cd $HOME
 #trace -t dirección ip
 
 echo "$(tput setaf 3)Instalación Podman"
-tput reset
 sleep 3
+tput reset
 sudo apt update -y
 source /etc/os-release
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
@@ -64,13 +65,13 @@ sudo apt-get -qq --yes install podman
 
 
 echo "$(tput setaf 2)Configuración OpenSearch"
-tput reset
 sleep 3
+tput reset
 podman pod create -n miOS -p 9200:9200 -p 9600:9600 -p 5601:5601
 podman container create --pod=miOS -e "discovery.type=single-node" opensearchproject/opensearch:latest
 podman container create --pod=miOS -e "opensearch.username=admin" -e "opensearch.password=admin" -e "opensearch.ssl.verificationMode=none" opensearchproject/opensearch-dashboards:latest
 podman pod start miOS
 echo  "$(tput setaf 5) $(tput setab 10)Done."
-tput reset
 sleep 10
+tput reset
 clear
