37cd7d4456fd048f19bf7de320ceecc0de668e80
Jmatop <91370388+Jmatop@users.noreply.github.com>
2022-04-08 15:44:00 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,24 +8,32 @@ sudo apt update
 
 
 
-echo "$(tput setaf 4)Instalamos Suricata"
+tput setaf 4
+echo "Instalamos Suricata"
+tput reset
 sleep 3
 sudo add-apt-repository ppa:oisf/suricata-stable 
 sudo apt install --yes software-properties-common
 sudo apt install --yes suricata
-tput reset
 
-echo "$(tput setaf 5)Habilitamos Suricata"
-#sudo systemctl enable suricata.service
-#sudo suricata-update
-tput reset
 
-echo "$(tput setaf 6)Instalamos servidor Apache"
+tput setaf 5
+echo "Habilitamos Suricata"
+tput reset
+sudo systemctl enable suricata.service
+sudo suricata-update
+
+
+tput setaf 6
+echo "Instalamos servidor Apache"
+tput reset
 sleep 3
 sudo apt install --yes apache2
-tput reset
 
-echo "$(tput setaf 2)Instalamos las dependencias para Yara"
+
+tput setaf 2
+echo "Instalamos las dependencias para Yara"
+tput reset
 sleep 3
 sudo apt-get install -y automake libtool make gcc flex bison libssl-dev libjansson-dev libmagic-dev
 sudo apt-get install -y checkinstall
@@ -37,7 +45,9 @@ git clone https://github.com/Yara-Rules/rules.git
 #yara -f /home/rules/index.yar /home/calipso/
 
 
-echo "$(tput setaf 3)Instalamos el IP Tracer"
+tput setaf 3
+echo "Instalamos el IP Tracer"
+tput reset
 sleep 3
 git clone https://github.com/rajkumardusad/IP-Tracer.git
 cd IP-Tracer
@@ -47,7 +57,9 @@ cd $HOME
 #Ejemplo comandos IP Tracer:
 #trace -t dirección ip
 
-echo "$(tput setaf 3)Instalación Podman"
+tput setaf 3
+echo "Instalación Podman"
+tput reset
 sleep 3
 sudo apt update -y
 source /etc/os-release
@@ -56,17 +68,16 @@ wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:st
 sudo apt update -qq
 sudo apt-get -qq --yes install podman
 
-echo "Configurating OpenSearch."
-sleep 5 
 
-echo "$(tput setaf 2)Configuración OpenSearch"
+tput setaf 2
+echo "Configuración OpenSearch"
+tput reset
+sleep 3
 podman pod create -n miOS -p 9200:9200 -p 9600:9600 -p 5601:5601
-sleep 20
 podman container create --pod=miOS -e "discovery.type=single-node" opensearchproject/opensearch:latest
-sleep 20
 podman container create --pod=miOS -e "opensearch.username=admin" -e "opensearch.password=admin" -e "opensearch.ssl.verificationMode=none" opensearchproject/opensearch-dashboards:latest
-sleep 20
 podman pod start miOS
-echo "Done."
+echo  "$(tput setaf 5) $(tput setab 10)Done."
+tput reset
 sleep 10
 clear
