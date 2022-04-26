33135b34763e8e9375875afd3757928c66011696
Jmatop <91370388+Jmatop@users.noreply.github.com>
2022-04-26 16:24:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,7 +16,11 @@ sleep 3
 tput reset
 sudo apt update
 
-
+echo -e "\e[1;32m*******************************************"
+echo -e "*                                         *"
+echo -e "*           Instalación Suricata          *"
+echo -e "*                                         *"
+echo -e "*******************************************\e[0m"
 
 echo "$(tput setaf 4)Instalamos Suricata"
 sleep 3
@@ -32,14 +36,20 @@ tput reset
 sudo systemctl enable suricata.service
 sudo suricata-update
 
-
-echo "$(tput setaf 6)Instalamos servidor Apache"
-sleep 3
+echo -e "\e[1;32m*****************************************"
+echo -e "*                                       *"
+echo -e "*           Instalación Apache          *"
+echo -e "*                                       *"
+echo -e "*****************************************\e[0m"
 tput reset
 sudo apt install --yes apache2
 
 
-echo "$(tput setaf 2)Instalamos las dependencias para Yara"
+echo -e "\e[1;32m*****************************************"
+echo -e "*                                       *"
+echo -e "*         Instalación Reglas Yara       *"
+echo -e "*                                       *"
+echo -e "*****************************************\e[0m"
 sleep 3
 tput reset
 sudo apt-get install -y automake libtool make gcc flex bison libssl-dev libjansson-dev libmagic-dev
@@ -52,8 +62,11 @@ git clone https://github.com/Yara-Rules/rules.git
 #yara -f /home/rules/index.yar /home/calipso/
 
 
-echo "$(tput setaf 3)Instalamos el IP Tracer"
-sleep 3
+echo -e "\e[1;32m********************************************"
+echo -e "*                                          *"
+echo -e "*           Instalación IP Tracer          *"
+echo -e "*                                          *"
+echo -e "********************************************\e[0m"
 tput reset
 git clone https://github.com/rajkumardusad/IP-Tracer.git
 cd IP-Tracer
@@ -63,30 +76,26 @@ cd $HOME
 #Ejemplo comandos IP Tracer:
 #trace -t dirección ip
 
-echo "$(tput setaf 3)Instalación Podman"
-sleep 3
-tput reset
-sudo apt update -y
-source /etc/os-release
-sudo sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
-wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_${VERSION_ID}/Release.key -O- | sudo apt-key add -
-sudo apt update -qq
-sudo apt-get -qq --yes install podman
-sudo systemctl start podman
-sudo systemctl enable podman
-sudo systemctl stop ufw 
-sudo systemctl disable ufw
-
-
-
-echo "$(tput setaf 2)Configuración OpenSearch"
-sleep 3
-tput reset
-podman pod create -n miOS -p 9200:9200 -p 9600:9600 -p 5601:5601
-podman container create --pod=miOS -e "discovery.type=single-node" opensearchproject/opensearch:latest
-podman container create --pod=miOS -e "opensearch.username=admin" -e "opensearch.password=admin" -e "opensearch.ssl.verificationMode=none" opensearchproject/opensearch-dashboards:latest
-podman pod start miOS
-echo  "$(tput setaf 5) $(tput setab 10)Done."
-sleep 10
-tput reset
-clear
+echo -e "\e[1;32m***********************************************"
+echo -e "*                                             *"
+echo -e "*           Instalando ElasticSearch          *"
+echo -e "*                                             *"
+echo -e "***********************************************\e[0m"
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
+sudo apt-get install apt-transport-https
+echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
+sudo apt-get update && sudo apt-get install elasticsearch > prueba.txt
+sudo /bin/systemctl daemon-reload
+sudo /bin/systemctl enable elasticsearch.service
+sudo systemctl start elasticsearch.service
+pass=$(cat prueba.txt | grep "generated password for the elastic built-in superuser " | awk '{print $11}')
+echo "password-elastic:" $pass | sudo tee -a config-elastic.txt
+rm prueba.txt
+sleep 20
+sudo curl -s --cacert /etc/elasticsearch/certs/http_ca.crt -k -u elastic:$pass https://localhost:9200 | grep "You Know, for Search"  > /dev/null
+funciona=$?
+if (( $funciona == 0 )); then
+        echo -e "\e[1;32m ElasticSearch funciona correctamente !!!\e[0m"
+else
+        echo "No funciona"
+fi
