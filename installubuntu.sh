a40b73b8369bcd79ea54fe6c154f470469acc2d4
Jmatop <91370388+Jmatop@users.noreply.github.com>
2022-04-29 11:16:14 +0200

    Update installubuntu.sh

--- installubuntu.sh
+++ installubuntu.sh
@@ -75,29 +75,53 @@ cd $HOME
 #Ejemplo comandos IP Tracer:
 #trace -t dirección ip
 
+#!/bin/bash
+
 echo -e "\e[1;32m***********************************************"
 echo -e "*                                             *"
 echo -e "*           Instalando ElasticSearch          *"
 echo -e "*                                             *"
 echo -e "***********************************************\e[0m"
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
+sudo systemctl stop ufw
+sudo systemctl disable ufw
+sudo apt install default-jdk
 sudo apt-get install apt-transport-https
-echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
-sudo apt-get update && sudo apt-get install elasticsearch > prueba.txt
-sudo /bin/systemctl daemon-reload
-sudo /bin/systemctl enable elasticsearch.service
-sudo systemctl start elasticsearch.service
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
+sudo apt update
+echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo t>sudo apt-get update && sudo apt-get install elasticsearch > prueba.txt
+sudo systemctl daemon-reload
+sudo systemctl enable elasticsearch
+sudo systemctl start elasticsearch
 pass=$(cat prueba.txt | grep "generated password for the elastic built-in superuser " | awk '{print $11}')
 echo "password-elastic:" $pass | sudo tee -a config-elastic.txt
 rm prueba.txt
 sleep 20
-sudo curl -s --cacert /etc/elasticsearch/certs/http_ca.crt -k -u elastic:$pass https://localhost:9200 | grep "You Know, for Search"  > /dev/null
-
+sudo curl -s --cacert /etc/elasticsearch/certs/http_ca.crt -k -u elastic:$pass https://localhost:9200 | grep "You Know, for Search"  > >curl -X GET "localhost:9200/"
+sleep 10
 echo -e "\e[1;32m****************************************"
 echo -e "*                                      *"
 echo -e "*           Instalando Kibana          *"
 echo -e "*                                      *"
 echo -e "****************************************\e[0m"
-sudo apt-get install --yes kibana
-sudo systemctl start kibana
+sudo apt install kibana
+sudo systemctl daemon-reload
 sudo systemctl enable kibana
+sudo systemctl start kibana
+sudo systemctl status kibana
+sleep 10
+echo -e "\e[1;32m*******************************************"
+echo -e "*                                         *"
+echo -e "*           Configuración Kibana          *"
+echo -e "*                                         *"
+echo -e "*******************************************\e[0m"
+sudo sed -i 's/#server.host: "localhost"/server.host: "0.0.0.0"/' /etc/kibana/kibana.yml
+token=$(sudo /usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s kibana)
+echo "Token-kibana:" $token | sudo tee -a config-elastic.txt
+sudo /bin/systemctl daemon-reload
+sudo systemctl start kibana.service
+sudo /bin/systemctl enable kibana.service
+echo -e "\e[1;32mEsperando Codigo....\e[0m"
+sleep 10
+codigo=$(journalctl -u kibana.service | grep "code=" | awk '{print $8}')
+echo "Code Verification:" $codigo | sudo tee -a config-elastic.txt
+echo -e "\e[1;32m Ya está todo Instalado, para acabar de configurar Kibana, vaya a: Ip_Màquina:5601\e[0m"
