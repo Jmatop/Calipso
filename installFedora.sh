0c5a54dc263c908f60c833c1f482bca54123cefd
Jmatop <91370388+Jmatop@users.noreply.github.com>
2022-05-03 20:41:53 +0200

    Update installFedora.sh

--- installFedora.sh
+++ installFedora.sh
@@ -145,6 +145,7 @@ echo "[Unit]" | sudo tee -a /etc/systemd/system/tshark.service
 echo "Description=Tshark" | sudo tee -a /etc/systemd/system/tshark.service
 echo "[Service]" | sudo tee -a /etc/systemd/system/tshark.service
 echo "Type=simple" | sudo tee -a /etc/systemd/system/tshark.service
+//setenforce 0
 echo "ExecStart=/home/$USER/tshark.sh" | sudo tee -a /etc/systemd/system/tshark.service
 echo "[Install]" | sudo tee -a /etc/systemd/system/tshark.service
 echo "WantedBy=multi-user.target" | sudo tee -a /etc/systemd/system/tshark.service
